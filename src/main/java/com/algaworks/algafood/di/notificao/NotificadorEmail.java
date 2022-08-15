a5447909802e9a1ac24e71cae382230670c94db6
Leonardo Campos <leonardu03@gmail.com>
2022-08-15 16:03:54 -0300

    Especialista Springboot

---
+++ src/main/java/com/algaworks/algafood/di/notificao/NotificadorEmail.java
@@ -0,0 +1,16 @@
+package com.algaworks.algafood.di.notificao;
+
+import com.algaworks.algafood.di.modelo.Cliente;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificadorEmail {
+
+    
+
+    public void notificar(Cliente cliente, String mensagem) {
+        System.out.printf("Notificando %s através do e-mail %s: %s\n",
+                cliente.getNome(), cliente.getEmail(), mensagem);
+    }
+
+}
---
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -0,0 +1,19 @@
+package com.algaworks.algafood.di.service;
+
+import com.algaworks.algafood.di.modelo.Cliente;
+import com.algaworks.algafood.di.notificao.NotificadorEmail;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AtivacaoClienteService {
+
+    private NotificadorEmail notificador;
+
+
+
+    public void ativar(Cliente cliente){
+        cliente.ativar();
+
+        notificador.notificar(cliente,"Seu cadastro no sistema está ativo!");
+    }
+}
