2c5ee2d4e16c1a1021f1a3a09da16fab0f654a7e
Leonardo Campos <leonardu03@gmail.com>
2022-08-26 12:24:35 -0300

    Especialista Springboot

---
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -0,0 +1,30 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public List<Cozinha> listar(){
+        return cozinhaRepository.listar();
+    }
+
+    @GetMapping("/{cozinhaId}")
+    public Cozinha buscar(@PathVariable Long cozinhaId){
+        return cozinhaRepository.buscar(cozinhaId);
+
+    }
+
+}
