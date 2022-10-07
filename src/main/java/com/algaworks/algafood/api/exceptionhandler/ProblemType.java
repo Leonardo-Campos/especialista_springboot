7a0ab12ffd39d1429a7e6c5aa4e8fe2a3adcd461
Leonardo Campos <leonardu03@gmail.com>
2022-10-07 11:55:57 -0300

    Especialista Springboot

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 
+    DADOS_INVALIDOS("/dados-inválidos", "Dados inválidos"),
     ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema"),
     PARAMETRO_INVALIDO("/parametro-inválido", "Parametro inválido"),
     MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem imcompreensível"),
@@ -16,7 +17,7 @@ public enum ProblemType {
     private String title;
     private String uri;
 
-    ProblemType(String title, String path) {
+    ProblemType(String path, String title) {
         this.title = title;
         this.uri = "https://algafood.com.br" + path;
     }
