460e000dd72c6c77b1716cbc678a03dd002c5ce5
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2022-11-03 09:27:58 -0300

    Especialista Springboot

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -80,6 +80,18 @@ public class RestauranteController {
         }
     }
 
+    @PutMapping("/{restauranteId}/ativo")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void ativar( @PathVariable Long restauranteId) {
+        cadastroRestaurante.ativar(restauranteId);
+    }
+
+    @DeleteMapping("/{restauranteId}/ativo")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void inativar( @PathVariable Long restauranteId) {
+        cadastroRestaurante.inativar(restauranteId);
+    }
+
     /*
     Comentários abaixo foram feitos para registrar como fazer um PatchMapping (Atualização Parcial)
     @PatchMapping("/{restauranteId}")
