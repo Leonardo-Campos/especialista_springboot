b576578ba9239976ee8e1089d83346601a3cc746
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2022-11-08 09:10:09 -0300

    Especialista Springboot

---
+++ src/main/java/com/algaworks/algafood/api/model/CidadeResumoModel.java
@@ -0,0 +1,16 @@
+package com.algaworks.algafood.api.model;
+
+import com.algaworks.algafood.api.model.EstadoModel;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class CidadeResumoModel {
+
+    private Long id;
+    private String nome;
+    private String estado;
+
+}
+
