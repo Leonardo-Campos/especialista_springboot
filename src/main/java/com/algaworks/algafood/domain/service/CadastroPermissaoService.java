43e286beaff7461c017083b54c779280ee8cb879
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2022-11-11 10:40:35 -0300

    Especialista Springboot

--- src/main/java/com/algaworks/algafood/domain/service/CadastroPermissaoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroPermissaoService.java
@@ -1,14 +1,10 @@
 package com.algaworks.algafood.domain.service;
 
-import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
-import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.PermissaoNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Permissao;
 import com.algaworks.algafood.domain.repository.PermissaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CadastroPermissaoService {
@@ -16,25 +12,9 @@ public class CadastroPermissaoService {
     @Autowired
     private PermissaoRepository permissaoRepository;
 
-    @Transactional
-    public Permissao salvar(Permissao permissao) {
-        return permissaoRepository.save(permissao);
+    public Permissao buscarOuFalhar(Long permissaoId) {
+        return permissaoRepository.findById(permissaoId)
+                .orElseThrow(() -> new PermissaoNaoEncontradaException(permissaoId));
     }
-
-    @Transactional
-    public void excluir(Long permissaoId) {
-        try {
-            permissaoRepository.deleteById(permissaoId);
-            permissaoRepository.flush();
-
-        } catch (EmptyResultDataAccessException e) {
-            throw new EntidadeNaoEncontradaException(
-                    String.format("Não existe um cadastro de permissão com código %d", permissaoId));
-
-        } catch (DataIntegrityViolationException e) {
-            throw new EntidadeEmUsoException(
-                    String.format("Permissão de código %d não pode ser removida, pois está em uso", permissaoId));
-        }
-    }
-
 }
+
