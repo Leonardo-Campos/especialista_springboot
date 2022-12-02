9999f822da499ec78d4112a2b76f98b4d5d5bd36
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2022-12-02 12:00:14 -0300

    Especialista Springboot

--- src/main/java/com/algaworks/algafood/domain/service/VendaQueryService.java
+++ src/main/java/com/algaworks/algafood/domain/service/VendaQueryService.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 public interface VendaQueryService {
 
-    List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro);
+    List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro, String timeOffset);
 }
