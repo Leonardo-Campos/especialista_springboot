ef191d379d6bd72f24e801ea565a906197cf8b29
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2022-12-08 10:57:16 -0300

    Especialista Springboot

--- src/main/java/com/algaworks/algafood/domain/service/CatalogoFotoProdutoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CatalogoFotoProdutoService.java
@@ -6,14 +6,46 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.InputStream;
+import java.util.Optional;
+
 @Service
 public class CatalogoFotoProdutoService {
 
     @Autowired
     private ProdutoRepository produtoRepository;
 
+    @Autowired
+    private FotoStorageService fotoStorageService;
+
     @Transactional
-    public FotoProduto salvar(FotoProduto foto) {
-        return produtoRepository.save(foto);
+    public FotoProduto salvar(FotoProduto foto, InputStream dadosArquivo) {
+        Long restauranteId = foto.getRestauranteId();
+        Long produtoId = foto.getProduto().getId();
+        String nomeNovoArquivo = fotoStorageService.gerarNomeArquivo(foto.getNomeArquivo());
+        String nomeArquivoExistente = null;
+
+        Optional<FotoProduto> fotoExistente = produtoRepository
+                .findFotoById(restauranteId, produtoId);
+
+        if(fotoExistente.isPresent()){
+            nomeArquivoExistente = fotoExistente.get().getNomeArquivo();
+            produtoRepository.delete(fotoExistente.get());
+        }
+
+        foto.setNomeArquivo(nomeNovoArquivo);
+        foto = produtoRepository.save(foto);
+        produtoRepository.flush();
+
+        FotoStorageService.NovaFoto novaFoto = FotoStorageService.NovaFoto.builder()
+                .nomeArquivo(foto.getNomeArquivo())
+                .inputStream(dadosArquivo)
+                .build();
+
+
+        fotoStorageService.substituir(nomeArquivoExistente, novaFoto);
+
+        return foto;
+
     }
 }
---
+++ src/main/java/com/algaworks/algafood/domain/service/FotoStorageService.java
@@ -0,0 +1,32 @@
+package com.algaworks.algafood.domain.service;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.io.InputStream;
+import java.util.UUID;
+
+public interface FotoStorageService {
+
+    void armazenar(NovaFoto novaFoto);
+    void remover(String nomeArquivo);
+    default String gerarNomeArquivo(String nomeOriginal) {
+        return UUID.randomUUID().toString() + "_" + nomeOriginal;
+    }
+
+    default void substituir(String nomeArquivoAntigo, NovaFoto novaFoto){
+        this.armazenar(novaFoto);
+
+        if(nomeArquivoAntigo != null) {
+            this.remover(nomeArquivoAntigo);
+        }
+    }
+
+    @Getter
+    @Builder
+    class NovaFoto {
+
+        private String nomeArquivo;
+        private InputStream inputStream;
+    }
+}
