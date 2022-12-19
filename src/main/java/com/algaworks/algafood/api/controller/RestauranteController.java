b28a9ce274baec51a5d71a963ba346aa2b85ba28
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2022-12-19 09:41:24 -0300

    Especialista Springboot

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin(maxAge = 10)
 @RestController
 @RequestMapping(value = "/restaurantes")
 public class RestauranteController {
