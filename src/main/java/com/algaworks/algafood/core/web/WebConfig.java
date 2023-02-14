6dc44fcca232bf4b9c831ffd43d6a8d9361ac163
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2023-02-14 11:52:17 -0300

    Deprecation Handler

--- src/main/java/com/algaworks/algafood/core/web/WebConfig.java
+++ src/main/java/com/algaworks/algafood/core/web/WebConfig.java
@@ -1,16 +1,21 @@
 package com.algaworks.algafood.core.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private ApiDeprecationHandler apiDeprecationHandler;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -19,6 +24,10 @@ public class WebConfig implements WebMvcConfigurer {
 //                .allowedOrigins("*");
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(apiDeprecationHandler);
+    }
 
     @Bean
     public Filter shallowEtagHeaderFilter() {
