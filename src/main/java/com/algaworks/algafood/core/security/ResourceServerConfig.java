793c70cb7ab6f1727587e12466ecf49c26e7e5a2
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2023-03-03 14:34:21 -0300

    Cors Auth Application

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,28 +1,24 @@
 package com.algaworks.algafood.core.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@Order()
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-                http
-                        .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                        .cors().and()
-                .oauth2ResourceServer().opaqueToken();
-
-    }
-
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest().authenticated()
+			.and()
+			.cors().and()
+			.oauth2ResourceServer().opaqueToken();
+	}
+	
 }
