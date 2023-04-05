78a5ca82ee835515fb372539dd1e3054cae8a958
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2023-04-05 08:12:14 -0300

    Documentação de endpoints de cozinhas

--- src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
+++ src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
@@ -66,7 +66,13 @@ public class SpringDocConfig {
                 .tags(Arrays.asList(
                         new Tag().name("Cidades").description("Gerencia as cidades"),
                         new Tag().name("Grupos").description("Gerencia os grupos")
-                )).components(new Components()
+                ))
+                .tags(Arrays.asList(
+                        new Tag().name("Cidades").description("Gerencia as cidades"),
+                        new Tag().name("Grupos").description("Gerencia os grupos"),
+                        new Tag().name("Cozinhas").description("Gerencia as cozinhas")
+                ))
+                .components(new Components()
                         .schemas(gerarSchemas())
                         .responses(gerarResponses())
                 );
