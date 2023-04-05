93f812a17c847f5e7cd0f884bf3e71c4b4d8cc5d
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2023-04-05 08:33:30 -0300

    Documentação de endpoints de pedidos

--- src/main/java/com/algaworks/algafood/api/v1/model/input/ItemPedidoInput.java
+++ src/main/java/com/algaworks/algafood/api/v1/model/input/ItemPedidoInput.java
@@ -1,8 +1,10 @@
 package com.algaworks.algafood.api.v1.model.input;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
 
@@ -10,12 +12,15 @@ import javax.validation.constraints.PositiveOrZero;
 @Getter
 public class ItemPedidoInput {
 
+    @Schema(example = "1")
     @NotNull
     private Long produtoId;
 
+    @Schema(example = "2")
+    @Min(1)
     @NotNull
-    @PositiveOrZero
     private Integer quantidade;
 
+    @Schema(example = "Menos picante, por favor")
     private String observacao;
 }
